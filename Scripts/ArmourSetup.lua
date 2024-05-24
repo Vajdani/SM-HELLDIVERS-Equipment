e5416d9d3baa54613d17e827454258828e1b2b86
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-05-24 02:56:49 +0200

    Moved rends back to characters folder

--- Scripts/ArmourSetup.lua
+++ Scripts/ArmourSetup.lua
@@ -5,7 +5,7 @@ local armour = {
     {
         uuid = sm.uuid.new("d39d911c-280b-429e-8168-2c6db39c4eaf"),
         slot = "head",
-        renderable = "$CONTENT_DATA/Objects/Renderables/char_helldiver_armour_helmet.rend",
+        renderable = "$CONTENT_DATA/Characters/Renderable/char_helldiver_armour_helmet.rend",
         stats = {
             damageReduction = 0.15
         },
@@ -14,7 +14,7 @@ local armour = {
     {
         uuid = sm.uuid.new("f4aada57-e8ac-4a54-ba79-6cdb9d73f039"),
         slot = "torso",
-        renderable = "$CONTENT_DATA/Objects/Renderables/char_helldiver_armour_chestplate.rend",
+        renderable = "$CONTENT_DATA/Characters/Renderable/char_helldiver_armour_chestplate.rend",
         stats = {
             damageReduction = 0.25
         },
@@ -23,7 +23,7 @@ local armour = {
     {
         uuid = sm.uuid.new("2592e8f0-2bab-4f4f-b153-1566717ab42e"),
         slot = "leg",
-        renderable = "$CONTENT_DATA/Objects/Renderables/char_helldiver_armour_leggings.rend",
+        renderable = "$CONTENT_DATA/Characters/Renderable/char_helldiver_armour_leggings.rend",
         stats = {
             damageReduction = 0.25
         },
@@ -32,7 +32,7 @@ local armour = {
     {
         uuid = sm.uuid.new("37eac317-fff1-47b7-86d0-e67c31cdf09a"),
         slot = "foot",
-        renderable = "$CONTENT_DATA/Objects/Renderables/char_helldiver_armour_boots.rend",
+        renderable = "$CONTENT_DATA/Characters/Renderable/char_helldiver_armour_boots.rend",
         stats = {
             damageReduction = 0.15
         },
